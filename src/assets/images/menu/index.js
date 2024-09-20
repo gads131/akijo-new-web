6db0d8695f19dc8cdf172ca2b682a1e03be513dc
gads131 <yosuabastian120@gmail.com>
2024-09-20 11:32:20 +0700

    remove unused image

--- src/assets/images/menu/index.js
+++
@@ -1,5 +0,0 @@
-import menu1 from "./bakso1.jpg";
-import menu2 from "./bakso2.png";
-import menu3 from "./bakso3.png";
-
-export { menu1, menu2, menu3 };
--- src/assets/images/product/index.js
+++
@@ -1,6 +0,0 @@
-import bakso from "./baksoba.jpg";
-import ayamgoreng from "./ayamgoreng.png";
-import porkslice from "./porkslice.jpeg";
-import siomay from "./siomay.jpg";
-
-export { bakso, ayamgoreng, porkslice, siomay };
